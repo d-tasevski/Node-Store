57e9408064547ae42ea9d9cd034c8e1867829ab9
Puritanic <darktasevski@gmail.com>
2017-11-01 21:03:59 +0100

    Add home controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const storeController = require('../controllers/storeController');
+
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get('/', storeController.homePage, (req, res) => {
+    res.render('hello');
 });
 
 module.exports = router;
